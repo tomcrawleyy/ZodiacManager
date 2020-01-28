322bb04c4d51685bbd7d2a3a6185432de7c4fef4
BuildTools <unconfigured@null.spigotmc.org>
2020-01-27 20:34:02 -0600

    Add test cases for HashMapLeaderboard

--- src/com/zodiacmc/ZodiacManager/Leaderboards/LeaderboardTest.java
+++ src/com/zodiacmc/ZodiacManager/Leaderboards/LeaderboardTest.java
@@ -1,24 +1,30 @@
 package com.zodiacmc.ZodiacManager.Leaderboards;
 
+import java.util.Comparator;
+import java.util.Map;
+import java.util.Map.Entry;
+
 class LeaderboardTest {
 
     private static boolean failed;
 
-
-    private LeaderboardTest() { }
-
+    private LeaderboardTest() {
+    }
 
     public static void main(String[] args) {
         // Test the ArrayLeaderboard class.
-        test(new ArrayLeaderboard<String, Integer>());
+        // test(new ArrayLeaderboard<String, Integer>());
 
         // Test the LinkedLeaderbaord class.
         // test(new LinkedLeaderboard<String, Integer>());
-    }
 
+        // Test the HashMapLeaderboard class.
+        testHashMapLeaderbaord();
+    }
 
     /**
      * Fuck me.
+     * 
      * @param lb fuck
      * @return me
      */
@@ -33,15 +39,8 @@ class LeaderboardTest {
         lb.add("1337", 80);
 
         // The first toString should be equal to this
-        String ts1 = "1. FooBar: 96\n"
-                    + "2. 1337: 80\n"
-                    + "3. Foo: 64\n"
-                    + "4. Bar: 48\n"
-                    + "5. PajamaMan: 32\n"
-                    + "6. Banana: 16\n"
-                    + "7. CoolGuy: 0\n"
-                    + "8. Toaster: -16";
-
+        String ts1 = "1. FooBar: 96\n" + "2. 1337: 80\n" + "3. Foo: 64\n" + "4. Bar: 48\n" + "5. PajamaMan: 32\n"
+                + "6. Banana: 16\n" + "7. CoolGuy: 0\n" + "8. Toaster: -16";
 
         if (!lb.toString().equals(ts1)) {
             System.out.println("Expected:\n" + ts1);
@@ -80,11 +79,10 @@ class LeaderboardTest {
         ILeaderboard.Entry<String, Integer> rm4 = lb.remove("Its 2:19 AM rn");
 
         // Its so fucking late. ;p
-        if ((!rm1.getKey().equals("Toaster") && rm1.getValue() != -16) ||
-            (!rm2.getKey().equals("CoolGuy") && rm2.getValue() != 0) ||
-            (!rm3.getKey().equals("1337") && rm3.getValue() != 80) || 
-            (rm4 != null)) {
-            
+        if ((!rm1.getKey().equals("Toaster") && rm1.getValue() != -16)
+                || (!rm2.getKey().equals("CoolGuy") && rm2.getValue() != 0)
+                || (!rm3.getKey().equals("1337") && rm3.getValue() != 80) || (rm4 != null)) {
+
             failure("*** Error in remove(K) method.");
         } else {
             if (lb.size() == 5) {
@@ -106,9 +104,8 @@ class LeaderboardTest {
         ILeaderboard.Entry<String, Integer> ge1 = lb.getEntry(4);
         ILeaderboard.Entry<String, Integer> ge2 = lb.getEntry(1);
         ILeaderboard.Entry<String, Integer> ge3 = lb.getEntry(67);
-        if ((!ge1.getKey().equals("FooBar") && ge1.getValue() != 96) ||
-            (!ge2.getKey().equals("PajamaMan") && ge2.getValue() != 32) ||
-            (ge3 != null)) {
+        if ((!ge1.getKey().equals("FooBar") && ge1.getValue() != 96)
+                || (!ge2.getKey().equals("PajamaMan") && ge2.getValue() != 32) || (ge3 != null)) {
             failure("*** Error in getEntry(int) method.");
         } else {
             System.out.println("getEntry(int) method is correct!");
@@ -119,8 +116,100 @@ class LeaderboardTest {
             failure("*** Error in clear() method.");
         }
 
-
         return failed;
+    }
+
+    public static void testHashMapLeaderbaord() {
+        HashMapLeaderboard<String, Integer> lb = new HashMapLeaderboard<>();
+
+        lb.put("Foo", 64);
+        lb.put("Bar", 48);
+        lb.put("FooBar", 96);
+        lb.put("Banana", 16);
+        lb.put("CoolGuy", 0);
+        lb.put("PajamaMan", 32);
+        lb.put("Toaster", -16);
+        lb.put("1337", 80);
+
+        lb.sort(new Comparator<Map.Entry<String, Integer>>() {
+
+            @Override
+            public int compare(Entry<String, Integer> o1, Entry<String, Integer> o2) {
+                return o2.getValue() - o1.getValue();
+            }
+
+        });
+
+        System.out.println(lb);
+
+        if (lb.size() != 8)
+            return;
+
+        lb.clear();
+        if (lb.size() != 0)
+            return;
+
+        lb.keyConstraint = t -> {
+            return t.length() > 6;
+        };
+
+        lb.put("Foo", 64);
+        lb.put("Bar", 48);
+        lb.put("FooBar", 96);
+        lb.put("Banana", 16);
+        lb.put("CoolGuy", 0);
+        lb.put("PajamaMan", 32);
+        lb.put("Toaster", -16);
+        lb.put("1337", 80);
+
+        System.out.println(lb);
+        if (lb.size() != 3)
+            return;
+
+        lb.clear();
+
+        lb.keyConstraint = null;
+        lb.valueConstraint = t -> {
+            return t >= 0;
+        };
+
+        lb.put("Foo", 64);
+        lb.put("Bar", 48);
+        lb.put("FooBar", 96);
+        lb.put("Banana", 16);
+        lb.put("CoolGuy", 0);
+        lb.put("PajamaMan", 32);
+        lb.put("Toaster", -16);
+        lb.put("1337", 80);
+
+        System.out.println(lb);
+
+        if (lb.size() != 7)
+            return;
+
+        lb.clear();
+
+        lb.keyConstraint = t -> {
+            return t.length() > 6;
+        };
+
+        lb.put("Foo", 64);
+        lb.put("Bar", 48);
+        lb.put("FooBar", 96);
+        lb.put("Banana", 16);
+        lb.put("CoolGuy", 0);
+        lb.put("PajamaMan", 32);
+        lb.put("Toaster", -16);
+        lb.put("1337", 80);
+
+        System.out.println(lb);
+
+        if (lb.size() != 2)
+            return;
+
+        lb.clear();
+        lb.load("reichman2: 21345623\nTomCrawley_: 465645\nCrossover_Lucky: 6355432");
+        System.out.println(lb);
     }
 
 
