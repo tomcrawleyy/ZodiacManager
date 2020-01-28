9963f52f6abb677ac37c26aba1b20a507007c5c5
BuildTools <unconfigured@null.spigotmc.org>
2020-01-27 20:33:41 -0600

    Add alias for Americans.

--- src/com/zodiacmc/ZodiacManager/Utilities/StringUtil.java
+++ src/com/zodiacmc/ZodiacManager/Utilities/StringUtil.java
@@ -6,4 +6,12 @@ public class StringUtil {
 	public static String parseColours(String msg) {
 		return ChatColor.translateAlternateColorCodes('&', msg);
 	}
+
+	/**
+	 * Alias for StringUtil.parseColours(String)
+	 * @return A string with formatted colors
+	 */
+	public static String parseColors(String msg) {
+		return parseColours(msg);
+	}
 }
